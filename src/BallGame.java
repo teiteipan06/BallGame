f2b6974c9b487a9973576e468a5e54884effe33d
teiteipan06 <y7321543@gmail.com>
2023-02-02 22:03:53 +0900

    draw a ball

--- src/BallGame.java
+++ src/BallGame.java
@@ -3,6 +3,15 @@ import javax.swing.*;
 
 public class BallGame extends JFrame {
 
+  Image ball = Toolkit.getDefaultToolkit().getImage("images/ball.png");
+  Image desk = Toolkit.getDefaultToolkit().getImage("images/desk.png");
+
+  // 畫視窗的方法
+  public void paint(Graphics g){
+    System.out.println("視窗畫一次！");
+    g.drawImage(ball, 100, 100, null);
+  }
+
   //窗口下載
   void launchFrame(){
     setSize(300, 300);
